5994a3583317d04271b398bfe097b1a4208adc3c
Salman H <Salman0112358@outlook.com>
2022-07-18 19:35:23 +0100

    refactoring to arrow functions

--- src/add.ts
+++ src/add.ts
@@ -5,9 +5,9 @@
  * @param secondNum second number to add
  * @returns total of the two numbers
  */
-function add(firstNum: number, secondNum: number): number {
-  const total = firstNum + secondNum;
-  return total;
+const add = (firstNum: number, secondNum: number): number => {
+  const total : number = firstNum + secondNum
+  return total
 }
 
 export default add;
